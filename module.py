6d4ad7f2c4213ed32ef343ec9848217b7975e08f
Aaron Hall, MBA <aaronchall@yahoo.com>
2016-01-28 21:48:25 -0500

    Update module.py

--- module.py
+++ module.py
@@ -7,3 +7,21 @@ If the module is imported, and help(module) called, it will show this as the hel
 >>> help(module)
 This is a module level docstring...
 """
+
+def a_function():
+    """
+    This is a function level docstring.
+    This function takes no arguments, and returns None
+    All functions return None unless they explicitly return something else.
+    
+    You can see the help on this function just like you can a module-level docstring:
+    
+    >>> help(a_function):
+    This is a function level ...
+    """
+
+def another_function(arg_1, arg2=None):
+    """
+    This function takes a required argument, arg_1, 
+    and an optional argument, arg_2, which defaults to None.
+    """
