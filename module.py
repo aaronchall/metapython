ff78fd1fff108fb0ca80d84e139ce1bde5acae2c
Aaron Hall, MBA <aaronchall@yahoo.com>
2016-01-28 21:39:36 -0500

    create module.py with docstring

---
+++ module.py
@@ -0,0 +1,9 @@
+"""
+This is a module level docstring. It documents the module, module.py.
+
+If the module is imported, and help(module) called, it will show this as the help. i.e.
+
+>>> import module
+>>> help(module)
+This is a module level docstring...
+"""
